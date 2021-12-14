9ef5b0554d4c8d803515b32c0267a67b0ade16ae
thangzathang <thangzathang@gmail.com>
2021-12-15 10:57:16 +1100

    Added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+
+const secret = 'test';
+
+const auth = async (req, res, next) => {
+  // We check if the user's token is valid.
+  try {
+    // Grabbing the token from req.body
+    const token = req.headers.authorization.split(' ')[1];
+    const ourAuth = token.length < 500;
+
+    let decodedData;
+
+    // Working with User's Token
+    if (token && ourAuth) {
+      decodedData = jwt.verify(token, secret);
+
+      req.userId = decodedData?.id;
+    } else {
+      // Working with GoogleAuth's Token if we decide to use Google Auth.
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }
+    // Auth middleware gives permission
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
--- routes/posts.js
+++ routes/posts.js
@@ -9,17 +9,17 @@ import {
 
 // Auth Middleware
 // Auth is applied for creating Post.
-// import auth from '../middleware/auth.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 
-router.post('/', createPost);
+router.post('/', auth, createPost);
 
-router.patch('/:id', updatePost);
+router.patch('/:id', auth, updatePost);
 
-router.delete('/:id', deletePost);
+router.delete('/:id', auth, deletePost);
 
 // router.patch('/:id/likePost', auth, likePost);
 
